78406359b6b2b292136923da11fd52cd0d193277
Tianyi Liu <liuty10@gmail.com>
2018-12-30 20:40:50 -0600

    deal with VirtualGL 32 bits and 64 bits issues

--- build32.sh
+++ build32.sh
@@ -5,16 +5,9 @@ export CXXFLAGS=-m32
 export LDFLAGS=-m32
 sudo apt-get install g++-multilib libxv-dev:i386 libxtst-dev:i386 libglu1-mesa-dev:i386
 cd cmake_build
+sudo rm /opt/libjpeg-turbo -rf
 sudo cp /opt/libjpeg-turbo.32 /opt/libjpeg-turbo -rf
 cmake -G"Unix Makefiles" ../
 make -j16
-sudo make install
-#mv ./lib ./lib32
+sudo make deb
 
-#rm client CMakeCache.txt CMakeFiles cmake_install.cmake cmake_uninstall.cmake -rf
-#export CFLAGS=-m64
-#export CXXFLAGS=-m64
-#export LDFLAGS=-m64
-#sudo apt-get install libxv-dev libxtst-dev libglu1-mesa-dev
-#cmake -G"Unix Makefiles" ../
-#make
