0766ed14b9782529a473415a1c2366065797eb75
liuty10 <liuty10@gmail.com>
2020-08-20 14:39:41 -0500

    setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
+#/bin/bash
 sudo apt-get install libxv-dev libxtst-dev libglu1-mesa-dev freeglut3-dev
-wget https://sourceforge.net/projects/libjpeg-turbo/files/2.0.0/libjpeg-turbo-official_2.0.0_amd64.deb
-sudo dpkg -i libjpeg-turbo-official_2.0.0_amd64.deb
+folder=/opt/libjpeg-turbo
+if [ -e "$folder" ] then
+	echo "libjpeg-turbo exits!"
+else
+	wget https://sourceforge.net/projects/libjpeg-turbo/files/2.0.0/libjpeg-turbo-official_2.0.0_amd64.deb
+	sudo dpkg -i libjpeg-turbo-official_2.0.0_amd64.deb
+	echo "configuration Done!"
+fi
+
+echo "If your client do not have libjpeg-turbo, please also install it on your client machine!"
