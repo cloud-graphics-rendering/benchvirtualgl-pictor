247b501ca113689ae68c6a57276f0b6856bc1b67
liuty10 <liuty10@gmail.com>
2020-08-20 11:34:37 -0500

    update setup and compiling scripts

--- build64.sh
+++ build64.sh
@@ -1,12 +1,10 @@
+sudo dpkg -r virtualgl
 rm ./cmake_build -rf
 mkdir ./cmake_build
 cd cmake_build
 export CFLAGS=-m64
 export CXXFLAGS=-m64
 export LDFLAGS=-m64
-sudo apt-get install libxv-dev libxtst-dev libglu1-mesa-dev
-sudo rm /opt/libjpeg-turbo -rf
-sudo cp /opt/libjpeg-turbo.64 /opt/libjpeg-turbo -rf
 cmake -G"Unix Makefiles" ../
 make -j16
 sudo make deb
---
+++ setup.sh
@@ -0,0 +1,3 @@
+sudo apt-get install libxv-dev libxtst-dev libglu1-mesa-dev freeglut3-dev
+wget https://sourceforge.net/projects/libjpeg-turbo/files/2.0.0/libjpeg-turbo-official_2.0.0_amd64.deb
+sudo dpkg -i libjpeg-turbo-official_2.0.0_amd64.deb
